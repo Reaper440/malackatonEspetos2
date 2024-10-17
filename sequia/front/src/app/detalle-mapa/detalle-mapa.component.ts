b081af7203e0a9c5f744c8a38ad59c37a64a7266
IreneGigli <irenegigli@uma.es>
2024-10-17 13:20:17 +0200

    cambios de localizacion

---
+++ sequia/front/src/app/detalle-mapa/detalle-mapa.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-mapa',
+  standalone: true,
+  imports: [],
+  templateUrl: './detalle-mapa.component.html',
+  styleUrl: './detalle-mapa.component.css'
+})
+export class DetalleMapaComponent {
+
+}
